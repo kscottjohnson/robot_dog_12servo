e8411d5d3e157ba79c187e6473fdb9b89603c5d6
Kevin Johnson <kscottjohnson@hotmail.com>
2024-02-25 15:42:41 -0600

    updated servo calibration

--- rd12/rd12.ino
+++ rd12/rd12.ino
@@ -17,16 +17,16 @@ Adafruit_PWMServoDriver driverR = Adafruit_PWMServoDriver(0x41);
 
 LegServo servos[12] = { 
   LegServo(&driverL, 0, 343,  90), // Front Left Hip - 0 degrees is down, 343
-  LegServo(&driverL, 1, 154,  90), // Front Left Shoulder - 0 degrees is forward
-  LegServo(&driverL, 2, 164,  12), // Front Left Knee - 0 degrees is up 164
-  LegServo(&driverR, 0, 343,  90), // Front Right Hip - 0 degrees is up
-  LegServo(&driverR, 1, 532,  90), // Front Right Shoulder - 0 degrees is back
-  LegServo(&driverR, 2, 521, 168), // Front Right Knee - 0 degrees is down
-  LegServo(&driverL, 4, 343,  90), // Back Left Hip 
-  LegServo(&driverL, 5, 154,  90), // Back Left Shoulder 
-  LegServo(&driverL, 6, 164,  12), // Back Left Knee 
-  LegServo(&driverR, 4, 343,  90), // Back Right Hip 
-  LegServo(&driverR, 5, 532,  90), // Back RightShoulder 
+  LegServo(&driverL, 1, 142,  90), // Front Left Shoulder - 0 degrees is forward
+  LegServo(&driverL, 2, 172,  12), // Front Left Knee - 0 degrees is up 164
+  LegServo(&driverR, 0, 346,  90), // Front Right Hip - 0 degrees is up
+  LegServo(&driverR, 1, 518,  90), // Front Right Shoulder - 0 degrees is back
+  LegServo(&driverR, 2, 515, 168), // Front Right Knee - 0 degrees is down
+  LegServo(&driverL, 4, 355,  90), // Back Left Hip 
+  LegServo(&driverL, 5, 152,  90), // Back Left Shoulder 
+  LegServo(&driverL, 6, 168,  12), // Back Left Knee 
+  LegServo(&driverR, 4, 337,  90), // Back Right Hip 
+  LegServo(&driverR, 5, 542,  90), // Back RightShoulder 
   LegServo(&driverR, 6, 521, 168)  // Back Right Knee 
 };
 
@@ -58,8 +58,6 @@ void loop() {
     delay(10);
   } 
   delay(1000);
-
-  //servos[0].setAngle(90);
 
 
 }
